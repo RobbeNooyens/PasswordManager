ac176964405d470670d8165a6275cce513594057
Robbe Nooyens <robbenooyens@hotmail.com>
2020-09-24 16:50:15 +0200

    Added length

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import Hashids from "hashids";
 var service = document.getElementById("service");
 var password = document.getElementById("password");
 var result = document.getElementById("result");
+var passLength = document.getElementById("length");
 
 function updateResult(){
     var rand = tychei(service["value"] + password["value"]);
     var seed = rand.quick() * 1000000;
-    result.innerHTML = new Hashids('', 8, 'abcdefghijklmnopqrstuvwxyz1234567890').encode(Math.round(seed));
+    result.innerHTML = new Hashids('', +passLength['value'], 'abcdefghijklmnopqrstuvwxyz1234567890').encode(Math.round(seed));
 }
 
 service.addEventListener("keyup", updateResult);
 password.addEventListener("keyup", updateResult);
+passLength.addEventListener("keyup", updateResult);
